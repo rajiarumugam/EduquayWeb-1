85129b47331d706723c3e9f7475b5f10d3ad6742
pooja <poojaraghup19@gmail.com>
2020-07-11 19:29:51 +0530

    Changed all the hard coded datas

--- EduquayWeb/ClientApp/src/app/shared/anm-module/shipmentlog/shipmentlog-resolver.service.ts
+++ EduquayWeb/ClientApp/src/app/shared/anm-module/shipmentlog/shipmentlog-resolver.service.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { ShipmentlogService } from './shipmentlog.service';
 import { ShipmentRequest } from './shipment-request';
+import { TokenService } from '../../token.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +13,17 @@ export class ShipmentlogResolverService implements Resolve<any>{
   shipmentResponse;
   shipmentRequest: ShipmentRequest
   constructor(
-    private ShipmentLogService: ShipmentlogService
+    private ShipmentLogService: ShipmentlogService,
+    private tokenService: TokenService
   ) { }
 
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<any> | Promise<any> | any {
-    this.shipmentRequest = {userId: 1, shipmentFrom: 4 };
+    var user = JSON.parse(this.tokenService.getUser('lu'));
+
+    this.shipmentRequest = {userId: user.id, shipmentFrom: user.shipmentFrom };
 
     return this.ShipmentLogService.getshipmentLog(this.shipmentRequest).pipe(
       catchError(error => {
