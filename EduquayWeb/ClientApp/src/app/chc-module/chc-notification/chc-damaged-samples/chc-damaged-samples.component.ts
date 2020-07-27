558ac8c6a9209a657eb1298e4ea2665a6e2e2694
pooja <poojaraghup19@gmail.com>
2020-07-27 10:20:38 +0530

    Changed Collection of date & time to Date & Time of Collection

---
+++ EduquayWeb/ClientApp/src/app/chc-module/chc-notification/chc-damaged-samples/chc-damaged-samples.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chc-damaged-samples',
+  templateUrl: './chc-damaged-samples.component.html',
+  styleUrls: ['./chc-damaged-samples.component.css']
+})
+export class ChcDamagedSamplesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
