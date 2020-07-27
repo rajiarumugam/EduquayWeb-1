1cbdc33f32a11d7cebf825a9d8aed31cd2c7bdcd
Suresh <pksuresh1985@gmail.com>
2020-07-27 19:59:58 +0530

    Central Sample Pcpr and HPLC and Molecular sample rcpt design completed

--- EduquayWeb/ClientApp/src/app/app.routing.module.ts
+++ EduquayWeb/ClientApp/src/app/app.routing.module.ts
@@ -78,6 +78,15 @@ import { ChcShipmentlogComponent } from "./chc-module/chc-shipmentlog/chc-shipme
 import { ChcShipmentlogResolverService } from "./shared/chc-module/chc-shipmentlog/chc-shipmentlog-resolver.service";
 import { ChcViewShipmentdetailsComponent } from "./chc-module/chc-view-shipmentdetails/chc-view-shipmentdetails.component";
 
+
+import { CentralSampleRcptMainComponent } from "./central-lab/sample-rcpt/central-sample-rcpt-main/central-sample-rcpt-main.component";
+import { CentralSampleRcptComponent } from "./central-lab/sample-rcpt/central-sample-rec/central-sample-rec.component";
+import { CentralHPLCupdateComponent } from "./central-lab/HPLC-update/central-update-hplc/central-update-hplc.component";
+import { CentralReceivedSampleComponent } from "./central-lab/HPLC-update/central-update-hplc-received/central-update-hplc-received.component";
+import { CentralHPLCUploadComponent } from "./central-lab/HPLC-update/central-update-hplc-upload/central-update-hplc-upload.component";
+
+import { MolecularSampleRcptMainComponent } from "./molecular-lab/sample-rcpt/molecular-sample-rcpt-main/molecular-sample-rcpt-main.component";
+import { MolecularSampleRcptComponent } from "./molecular-lab/sample-rcpt/molecular-sample-rec/molecular-sample-rec.component";
 const routes: Routes = [
   { path: 'home', component: HomeComponent, pathMatch: 'full' },
   { path: 'counter', component: CounterComponent },
@@ -148,6 +157,13 @@ const routes: Routes = [
         ]
       },
       {
+        path: 'central-update-hplc', component: CentralHPLCupdateComponent,
+        children:[
+          {path: '', component: CentralReceivedSampleComponent, pathMatch: 'full', resolve: {positiveSubjects: CHCupdateReceivedService}},
+          {path: 'upload', component: CentralHPLCUploadComponent, pathMatch: 'full', resolve: {positiveSubjects: CHCupdateReceivedService}}
+        ]
+      },
+      {
         path: 'anm-subregn', component: AnmSubjectRegistrationComponent,
         children:[
           {path: '', component: AnmAwRegistrationComponent, pathMatch: 'full'},
@@ -158,6 +174,18 @@ const routes: Routes = [
           {path: 'otherwalkin', component: AnmWalkinGt18RegistrationComponent, pathMatch: 'full'},
         ]
 
+      },
+      {
+        path: 'centrallab', component: CentralSampleRcptMainComponent,
+        children:[
+          {path: '', component: CentralSampleRcptComponent, pathMatch: 'full', resolve: {positiveSubjects: CHCSampleResolverService}}
+        ]
+      },
+      {
+        path: 'molecularlab', component: MolecularSampleRcptMainComponent,
+        children:[
+          {path: '', component: MolecularSampleRcptComponent, pathMatch: 'full', resolve: {positiveSubjects: CHCSampleResolverService}}
+        ]
       },
       { path: 'anm-sample-collection', component: SampleCollectionComponent, resolve: {sampleCollectionData: SampleCollectionResolverService} },
       { path: 'anm-sample-collection/:subtype', component: SampleCollectionComponent, resolve: {sampleCollectionData: SampleCollectionResolverService} },
@@ -234,5 +262,12 @@ export const RoutingComponents = [
   SSTUpdateNegativeComponent,
   ChcShipmentlogComponent,
   ChcViewShipmentdetailsComponent,
-  AssociatedANMComponent
+  AssociatedANMComponent,
+  CentralSampleRcptMainComponent,
+  CentralSampleRcptComponent,
+  CentralHPLCupdateComponent,
+  CentralReceivedSampleComponent,
+  CentralHPLCUploadComponent,
+  MolecularSampleRcptMainComponent,
+  MolecularSampleRcptComponent
 ];
