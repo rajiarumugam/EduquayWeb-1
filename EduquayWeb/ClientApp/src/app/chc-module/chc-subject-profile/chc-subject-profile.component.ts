9e874afd119ea2279f9254408cee5f630b4b7a40
pooja <poojaraghup19@gmail.com>
2020-07-29 09:36:49 +0530

    Removed testing code in chc damaged sample & positive Subjects

--- EduquayWeb/ClientApp/src/app/chc-module/chc-subject-profile/chc-subject-profile.component.ts
+++ EduquayWeb/ClientApp/src/app/chc-module/chc-subject-profile/chc-subject-profile.component.ts
@@ -6,6 +6,17 @@ import { SubjectProfileResponse, ReligionResponse, Religion, GovtIDTypeResponse,
 import { SubjectProfileService } from 'src/app/shared/anm-module/subject-profile/subject-profile.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import Swal from 'sweetalert2';
+import { ENDPOINT } from 'src/app/app.constant';
+import { HttpClientService } from 'src/app/shared/http-client.service';
+import { GenericService } from 'src/app/shared/generic.service';
+import { TokenService } from 'src/app/shared/token.service';
+import { masterService } from 'src/app/shared/master/district/masterdata.service';
+import { user } from 'src/app/shared/auth-response';
+declare var $: any;
+import * as moment from 'moment';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FlatpickrOptions } from 'ng2-flatpickr';
 
 @Component({
   selector: 'app-chc-subject-profile',
@@ -15,11 +26,14 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 export class ChcSubjectProfileComponent implements OnInit {
 
   @ViewChild('stepper', { static: false }) stepper: MatStepper;
+  @ViewChild('dobPicker', { static: false }) DOBPicker;
     
   chcsubjectProfileErrorMessage: string;
   chcsubjectProfileRequest: SubjectProfileRequest;
   chcsubjectProfileResponse: SubjectProfileResponse;
   chcreligionResponse: ReligionResponse;
+  user: user;
+  
   chcreligions: Religion[] = [];
   selectedreligion = '';
   chcgovtIdTypeResponse: GovtIDTypeResponse;
@@ -92,17 +106,35 @@ export class ChcSubjectProfileComponent implements OnInit {
   Ldisabled = true;
   Pdisabled = true;
   Adisabled = true;
+  selecteddob;
+  selectedage;
+  createdSubjectId="";
   GPLADATA = [{id:'00',value:'0'},{id:'1',value:'1'},{id:'2',value:'2'},{id:'3',value:'3'},{id:'4',value:'4'},{id:'5',value:'5'},{id:'6',value:'6'},{id:'7',value:'7'},{id:'8',value:'8'},{id:'9',value:'9'}];
 
+  startOptions1: FlatpickrOptions = {
+    mode: 'single',
+    dateFormat: 'd/m/Y',
+    defaultDate: "",
+    maxDate: new Date(Date.now()),
+  };
 
   constructor(
     private SubjectProfileService: SubjectProfileService,
     private modalService: NgbModal,
     private httpService: HttpClient,
-    private _formBuilder: FormBuilder
+    private _formBuilder: FormBuilder,
+    private httpClientService:HttpClientService,
+    private genericService: GenericService,
+    private tokenService: TokenService,
+    private masterService: masterService,
+    private router: Router,
+    private route: ActivatedRoute,
+
   ) { }
 
   ngOnInit() {
+
+    this.user = JSON.parse(this.tokenService.getUser('lu'));
     console.log(this.SubjectProfileService.subjectProfileApi);
 
     this.firstFormGroup = this._formBuilder.group({
@@ -348,6 +380,19 @@ export class ChcSubjectProfileComponent implements OnInit {
     );
   }
 
+  calculateAge()
+  {
+     var today = new Date();
+     var birthDate = new Date(this.selecteddob);
+     var age = today.getFullYear() - birthDate.getFullYear();
+     var m = today.getMonth() - birthDate.getMonth();
+     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+         age--;
+     }
+     this.selectedage = age;
+     //return age;
+  }
+
   // nextStep(stepper: MatStepper) {
   //   //this.firstFormCheck = true;
   //   //if(this.firstFormGroup.valid)
@@ -382,6 +427,139 @@ export class ChcSubjectProfileComponent implements OnInit {
       this.selecteda = "00";
       this.Ldisabled = false;
     }
+
+    formSubmit()
+  {
+    this.secondFormCheck = true;
+  
+    if(this.secondFormGroup.valid && this.firstFormGroup.valid)
+    {
+      var apiUrl = this.genericService.buildApiUrl(ENDPOINT.SUBJECT.ADD);
+      this.httpClientService.post<any>({url:apiUrl, body: this.dataBindinginServce() }).subscribe(response => {
+        this.createdSubjectId = response.uniqueSubjectId;
+        //this.getpositiveSubjectList();
+
+        Swal.fire({icon:'success', title: 'Subject ID is '+this.createdSubjectId,
+  showCancelButton: true, confirmButtonText: 'Collect sample now', cancelButtonText: 'Collect sample later' })
+     .then((result) => {
+       if (result.value) {
+        $('#fadeinModal').modal('hide');
+        this.router.navigateByUrl("app/chc-sample-collection");
+       
+       }
+       else{
+        this.firstFormGroup.reset();
+        this.secondFormGroup.reset();
+        this.secondFormCheck = false;
+        this.firstFormCheck = false;
+        this.stepper.selectedIndex = 0;
+        $('#fadeinModal').modal('hide');
+       }
+     });
+        //$('#fadeinModal').modal('show');
+        
+      },
+      (err: HttpErrorResponse) =>{
+        console.log(err);
+        this.chcsubjectProfileErrorMessage = err.toString();
+      });
+    }
+  }
+
+  dataBindinginServce()
+  {
+    var _obj = {
+      "subjectPrimaryRequest": {
+        "subjectTypeId": 1,
+        "childSubjectTypeId": 1,
+        "uniqueSubjectId": "",
+        "districtId": this.firstFormGroup.get('district').value != undefined ? Number(this.firstFormGroup.get('district').value) : 0,
+        "chcId": Number(this.firstFormGroup.get('chc').value),
+        // "phcId": Number(this.firstFormGroup.get('phc').value),
+        // "scId": Number(this.firstFormGroup.get('sc').value),
+        "riId": Number(this.secondFormGroup.get('ripoint').value),
+        "subjectTitle": this.firstFormGroup.get('subjectitle').value,
+        "firstName": this.firstFormGroup.get('firstname').value,
+        "middleName": this.firstFormGroup.get('middlename').value != undefined ? this.firstFormGroup.get('middlename').value : '',
+        "lastName": this.firstFormGroup.get('lastname').value,
+        "dob": this.firstFormGroup.get('dob').value != undefined ? moment(new Date(this.firstFormGroup.get('dob').value)).format("DD/MM/YYYY") : '',
+        "age": Number(this.firstFormGroup.get('age').value),
+        "gender": "Male",
+        "maritalStatus": true,
+        "mobileNo": ""+this.firstFormGroup.get('contactNumber').value,
+        "emailId": this.secondFormGroup.get('spouseEmail').value != undefined ? this.secondFormGroup.get('spouseEmail').value : '',
+        "govIdTypeId": this.secondFormGroup.get('govtIDType').value != undefined ? this.secondFormGroup.get('govtIDType').value : 0,
+        "govIdDetail": this.secondFormGroup.get('GovtIDDetail').value != undefined ? this.secondFormGroup.get('GovtIDDetail').value : '',
+        "spouseSubjectId": "",
+        "spouseFirstName": "",
+        "spouseMiddleName": "",
+        "spouseLastName": "",
+        "spouseContactNo": "",
+        "spouseGovIdTypeId": 0,
+        "spouseGovIdDetail": "",
+        "assignANMId": Number(this.secondFormGroup.get('associatedanm').value),
+        "dateOfRegister": moment(new Date(this.firstFormGroup.get('dor').value)).format("DD/MM/YYYY"),
+        "registeredFrom": Number(this.user.registeredFrom),
+        "createdBy": Number(this.user.id),
+        "source": "N"
+      },
+      "subjectAddressRequest": {
+        "religionId": Number(this.secondFormGroup.get('religion').value),
+        "casteId": Number(this.secondFormGroup.get('caste').value),
+        "communityId": Number(this.secondFormGroup.get('community').value),
+        "address1": this.secondFormGroup.get('house').value,
+        "address2": this.secondFormGroup.get('street').value,
+        "address3": this.secondFormGroup.get('city').value,
+        "pincode": ""+this.secondFormGroup.get('pincode').value,
+        "stateName": this.secondFormGroup.get('state').value,
+        "updatedBy": Number(this.user.id)
+      },
+      "subjectPregnancyRequest": {
+        "rchId": this.firstFormGroup.get('rchId').value,
+        "ecNumber": this.secondFormGroup.get('ECNumber').value,
+        "lmpDate": "",
+        "g": 0,
+        "p": 0,
+        "l": 0,
+        "a": 0,
+        "updatedBy": Number(this.user.id)
+      },
+      "subjectParentRequest": {
+        "motherFirstName": "",
+        "motherMiddleName": "",
+        "motherLastName": "",
+        "motherGovIdTypeId": 0,
+        "motherGovIdDetail": "",
+        "motherContactNo": "",
+        "fatherFirstName": "",
+        "fatherMiddleName": "",
+        "fatherLastName": "",
+        "fatherGovIdTypeId": 0,
+        "fatherGovIdDetail": "",
+        "fatherContactNo": "",
+        "gaurdianFirstName": "",
+        "gaurdianMiddleName": "",
+        "gaurdianLastName": "",
+        "gaurdianGovIdTypeId": 0,
+        "gaurdianGovIdDetail": "",
+        "gaurdianContactNo": "",
+        "rbskId": "",
+        "schoolName": "",
+        "schoolAddress1": "",
+        "schoolAddress2": "",
+        "schoolAddress3": "",
+        "schoolPincode": "",
+        "schoolCity": "",
+        "schoolState": "",
+        "standard": "",
+        "section": "",
+        "rollNo": "",
+        "updatedBy": Number(this.user.id)
+      }
+    };
+
+    return _obj;
+  }
 
 
 }
