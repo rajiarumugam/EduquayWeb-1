cd10b8f6eced0bb26fb69132d57d355e22a29eb2
pooja <poojaraghup19@gmail.com>
2020-11-06 19:50:27 +0530

    Added pdf option

--- EduquayWeb/ClientApp/src/app/chc-sample-module/repot-sample-status/repot-sample-status/repot-sample-status.component.ts
+++ EduquayWeb/ClientApp/src/app/chc-sample-module/repot-sample-status/repot-sample-status/repot-sample-status.component.ts
@@ -94,7 +94,7 @@ export class CHCreportSampleStatusComponent implements AfterViewInit, OnDestroy,
     this.getCHCData();
     //this.dataservice.sendData(JSON.stringify({"screen": "PNDTCTESTING","pendingCount":this.pndPendingArray.length}));
     this.dtOptions = {
-        
+    
       pagingType: 'simple_numbers',
       pageLength: 5,
       processing: true,
@@ -123,7 +123,9 @@ export class CHCreportSampleStatusComponent implements AfterViewInit, OnDestroy,
             extend: 'excelHtml5',
             title: 'Report - Sample Status',
             className: 'custom-btn',
-            text: '<img src="assets/assets/img/excelimage.png" width="23px" />'
+            text: '<img src="assets/assets/img/excelimage.png" width="23px" />',
+            //autoFilter: true,
+            
           },
         {
           titleAttr: 'Download as PDF',
@@ -131,15 +133,81 @@ export class CHCreportSampleStatusComponent implements AfterViewInit, OnDestroy,
           title: 'Report - Sample Status',
           orientation: 'landscape',
           pageSize: 'LEGAL',
-          className: 'custom-btn',
-          margin: [5,5,5,5],
-          //filename: 'dt_custom_pdf',
+          className: 'custom-btn',        
+          text: '<img src="../../../../assets/assets/img/pdfimage.png" width="23px" />',
+          customize: function(doc) {doc.styles.tableHeader.vertical = 'middle'
+               //Remove the title created by datatTables
+               
+						doc.content.splice(0,1);
+						//Create a date string that we use in the footer. Format is dd-mm-yyyy
+						var now = new Date();
+            var jsDate = now.getDate()+'-'+(now.getMonth()+1)+'-'+now.getFullYear();
+            doc.pageMargins = [20,60,20,30];
+						// Set the font size fot the entire document
+						doc.defaultStyle.fontSize = 10;
+						// Set the fontsize for the table header
+            doc.styles.tableHeader.fontSize = 11;
+            doc.styles.tableHeader.alignment = 'center'
+						// Create a header object with 3 columns
+						// Left side: Logo
+						// Middle: brandname
+						// Right side: A document title
+						doc['header']=(function() {
+							return {
+								columns: [
+									// {
+									// 	//image: logo,
+									// 	width: 24
+									// },
+									// {
+									// 	alignment: 'left',
+									// 	italics: true,
+									// 	text: 'dataTables',
+									// 	fontSize: 18,
+									// 	margin: [10,0]
+									// },
+									// {
+									// 	alignment: 'right',
+									// 	fontSize: 14,
+									// 	text: 'Custom PDF export with dataTables'
+									// }
+								],
+								margin: 20
+							}
+						});
+						// Create a footer object with 2 columns
+						// Left side: report creation date
+						// Right side: current page and total pages
+						doc['footer']=(function(page, pages) {
+							return {
+								columns: [
+									{
+										alignment: 'left',
+										text: ['Created on: ', { text: jsDate.toString() }]
+									},
+									{
+										alignment: 'right',
+										text: ['page ', { text: page.toString() },	' of ',	{ text: pages.toString() }]
+									}
+								],
+								margin: 20
+							}
+						});
+						// Change dataTable layout (Table styling)
+						// To use predefined layouts uncomment the line below and comment the custom lines below
+						// doc.content[0].layout = 'lightHorizontalLines'; // noBorders , headerLineOnly
+						var objLayout = {};
+						objLayout['hLineWidth'] = function(i) { return .5; };
+						objLayout['vLineWidth'] = function(i) { return .5; };
+						objLayout['hLineColor'] = function(i) { return '#aaa'; };
+						objLayout['vLineColor'] = function(i) { return '#aaa'; };
+						objLayout['paddingLeft'] = function(i) { return 4; };
+						objLayout['paddingRight'] = function(i) { return 4; };
+						doc.content[0].layout = objLayout;
+				},				
           exportOptions: {
-						columns: ':visible',
-						search: 'applied',
-						order: 'applied'
-					},
-          text: '<img src="../../../../assets/assets/img/pdfimage.png" width="23px" />'
+              columns: ':visible'
+          }
         },
         
         // {
--- EduquayWeb/ClientApp/src/app/molecular-lab/repot-sample-status/repot-sample-status/repot-sample-status.component.ts
+++ EduquayWeb/ClientApp/src/app/molecular-lab/repot-sample-status/repot-sample-status/repot-sample-status.component.ts
@@ -113,11 +113,79 @@ export class ReportSampleStatusComponent implements AfterViewInit, OnDestroy, On
            orientation: 'landscape',
            pageSize: 'LEGAL',
            className: 'custom-btn',
-           exportOptions: {
-             columns: ':visible',
-             search: 'applied',
-             order: 'applied'
-           },
+          customize: function(doc) {doc.styles.tableHeader.vertical = 'middle'
+               //Remove the title created by datatTables
+               
+						doc.content.splice(0,1);
+						//Create a date string that we use in the footer. Format is dd-mm-yyyy
+						var now = new Date();
+            var jsDate = now.getDate()+'-'+(now.getMonth()+1)+'-'+now.getFullYear();
+            doc.pageMargins = [20,60,20,30];
+						// Set the font size fot the entire document
+						doc.defaultStyle.fontSize = 10;
+						// Set the fontsize for the table header
+            doc.styles.tableHeader.fontSize = 11;
+            doc.styles.tableHeader.alignment = 'center'
+						// Create a header object with 3 columns
+						// Left side: Logo
+						// Middle: brandname
+						// Right side: A document title
+						doc['header']=(function() {
+							return {
+								columns: [
+									// {
+									// 	//image: logo,
+									// 	width: 24
+									// },
+									// {
+									// 	alignment: 'left',
+									// 	italics: true,
+									// 	text: 'dataTables',
+									// 	fontSize: 18,
+									// 	margin: [10,0]
+									// },
+									// {
+									// 	alignment: 'right',
+									// 	fontSize: 14,
+									// 	text: 'Custom PDF export with dataTables'
+									// }
+								],
+								margin: 20
+							}
+						});
+						// Create a footer object with 2 columns
+						// Left side: report creation date
+						// Right side: current page and total pages
+						doc['footer']=(function(page, pages) {
+							return {
+								columns: [
+									{
+										alignment: 'left',
+										text: ['Created on: ', { text: jsDate.toString() }]
+									},
+									{
+										alignment: 'right',
+										text: ['page ', { text: page.toString() },	' of ',	{ text: pages.toString() }]
+									}
+								],
+								margin: 20
+							}
+						});
+						// Change dataTable layout (Table styling)
+						// To use predefined layouts uncomment the line below and comment the custom lines below
+						// doc.content[0].layout = 'lightHorizontalLines'; // noBorders , headerLineOnly
+						var objLayout = {};
+						objLayout['hLineWidth'] = function(i) { return .5; };
+						objLayout['vLineWidth'] = function(i) { return .5; };
+						objLayout['hLineColor'] = function(i) { return '#aaa'; };
+						objLayout['vLineColor'] = function(i) { return '#aaa'; };
+						objLayout['paddingLeft'] = function(i) { return 4; };
+						objLayout['paddingRight'] = function(i) { return 4; };
+						doc.content[0].layout = objLayout;
+				},				
+          exportOptions: {
+              columns: ':visible'
+          },
            text: '<img src="../../../../assets/assets/img/pdfimage.png" width="23px" />'
          },
          ], 
