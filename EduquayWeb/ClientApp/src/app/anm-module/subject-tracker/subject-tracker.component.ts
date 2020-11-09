7a077c41d8c63115628e016d69795b22b275d4c6
pooja <poojaraghup19@gmail.com>
2020-11-09 21:47:32 +0530

    Added print icon to reports

---
+++ EduquayWeb/ClientApp/src/app/anm-module/subject-tracker/subject-tracker.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+declare var $: any 
+
+@Component({
+  selector: 'app-subject-tracker',
+  templateUrl: './subject-tracker.component.html',
+  styleUrls: ['./subject-tracker.component.css']
+})
+export class SubjectTrackerComponent implements OnInit {
+
+  constructor(
+    private modalService: NgbModal,
+  ) { }
+
+  ngOnInit() {
+  }
+
+  openchcSampleShipment(shippedChcSampleDetail){
+    $('#fadeinModal').modal('show');
+  
+    // this.modalService.open(
+    //   shippedChcSampleDetail,{
+    //     centered: true,
+    //     size: 'xl',
+    //     //windowClass: 'modal-xl',
+    //     scrollable: true,
+    //     backdrop:'static',
+    //     keyboard: false,
+    //     ariaLabelledBy: 'modal-basic-title'
+    //   });
+  }
+
+}
