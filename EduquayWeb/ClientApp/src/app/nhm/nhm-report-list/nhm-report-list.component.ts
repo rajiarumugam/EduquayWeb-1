bcaa4cc0334290f29794a36695283869f2b218a0
Suresh <pksuresh1985@gmail.com>
2020-12-26 21:47:04 +0530

    Print buttons enabled

--- EduquayWeb/ClientApp/src/app/nhm/nhm-report-list/nhm-report-list.component.ts
+++ EduquayWeb/ClientApp/src/app/nhm/nhm-report-list/nhm-report-list.component.ts
@@ -233,7 +233,7 @@ export class NHMreportListComponent implements AfterViewInit, OnDestroy, OnInit 
       stripeClasses: [],
       lengthMenu: [5, 10, 20, 50],
        // Declare the use of the extension in the dom parameter
-       /*dom: "<'row mt-3'<'col-sm-6 float left'f><'col-sm-4 mb-2 float right'l><'col-sm-2 float right'B>>" +
+       dom: "<'row mt-3'<'col-sm-6 float left'f><'col-sm-4 mb-2 float right'l><'col-sm-2 float right'B>>" +
        "<'row'<'col-sm-12'tr>>" +
        "<'row'<'col-sm-4'i><'col-sm-4 text-center'p>>",
        // Configure the buttons
@@ -343,7 +343,7 @@ export class NHMreportListComponent implements AfterViewInit, OnDestroy, OnInit 
          // text: ''
          // }
  
-         ], */
+         ], 
       language: {
         search: '<div><span class="note">Search by any Subject information from below</span></div><div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg></div>',
         searchPlaceholder: "Search...",
