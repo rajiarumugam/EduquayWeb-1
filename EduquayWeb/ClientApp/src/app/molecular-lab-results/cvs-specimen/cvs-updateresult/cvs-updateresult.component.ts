2a65c8570d551545fa2804f2e4b7ea861fdbd859
pooja <poojaraghup19@gmail.com>
2021-02-23 17:47:45 +0530

    updated menu name in molecular lab results

--- EduquayWeb/ClientApp/src/app/molecular-lab-results/cvs-specimen/cvs-updateresult/cvs-updateresult.component.ts
+++ EduquayWeb/ClientApp/src/app/molecular-lab-results/cvs-specimen/cvs-updateresult/cvs-updateresult.component.ts
@@ -48,7 +48,7 @@ export class CvsUpdateresultComponent implements AfterViewInit, OnDestroy, OnIni
 
   ngOnInit() {
 
-    this.dataservice.sendData(JSON.stringify({"module": "Molecular Lab", "submodule":"Update Molecular Test Results – CVS Specimen", "page": "Update Result"}));
+    this.dataservice.sendData(JSON.stringify({"module": "Update Molecular Test Results", "submodule":"CVS Specimen", "page": "Update Result"}));
     this.user = JSON.parse(this.tokenService.getUser('lu'));
     this.dtOptions = {
       pagingType: 'simple_numbers',
